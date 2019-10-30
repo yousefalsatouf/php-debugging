307ca25878f96c12a5da12fe7863844d0fa97667
Yousef Alsatouf <yousef.alsatouf94@gmail.com>
2019-10-30 13:31:29 +0100

    still working on them

--- expert.php
+++ expert.php
@@ -8,11 +8,175 @@ declare(strict_types=1);
 
 $x = 1;
 
-echo "Exercise ". $x ." starts here:";
+//echo "Exercise ". $x ." starts here:";
+
+new_exercise(1);
 
 function new_exercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise ". $x ." starts here:<br/>";
     echo $block;
 }
 
-new_exercise($x);
+new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = "Debugged ! Also very fun";
+echo substr($str, 0, 10);
+
+
+new_exercise(4);
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
+
+
+foreach($week as $day) {
+    $day = substr($day, 0, strlen($day)-3);
+
+    $week .= $day . " ";  
+
+  
+}
+
+print_r($week);
+
+new_exercise(5);
+// === Exercise 5 ===
+// The array should be printing every letter of the alfabet (a-z) but instead it does that + aa-yz
+// Fix the code so the for loop only pushes a-z in the array
+
+$arr = [];
+for ($letter = 'a'; $letter <= 'z'; $letter++) {
+    
+    // if ( sizeof($arr) <= 25){
+    //     array_push($arr, $letter);
+    // }
+
+    array_push($arr, $letter);
+    if($letter == "z") break;
+   
+}
+print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
+
+
+new_exercise(6);
+// === Final exercise ===
+// The fixed code should echo the following at the bottom:
+// Here is the name: $name - $name2
+// $name variables are decided as seen in the code, fix all the bugs whilst keeping the functionality!
+$arr = [];
+
+
+function combineNames($str1 = "", $str2 = "") {
+    $params = [$str1, $str2];
+    foreach($params as $param) {
+        if ($param == "") {
+            $param = randomHeroName();
+        }
+    }
+    echo implode($params, " - ");
+}
+
+
+function randomGenerate($arr, $amount) {
+    for ($i = $amount; $i > 0; $i--) {
+        array_push($arr, randomHeroName());
+    }
+
+    return $amount;
+}
+
+function randomHeroName()
+{
+    $hero_firstnames = ["captain", "doctor", "iron", "Hank", "ant", "Wasp", "the", "Hawk", "Spider", "Black", "Carol"];
+    $hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil", "marvel"];
+    $heroes = [$hero_firstnames, $hero_lastnames];
+    $randname = $heroes[rand(0,count($heroes))][rand(0, 10)];
+
+    echo $randname;
+}
+
+echo "Here is the name: " . combineNames();
+
+
+
+new_exercise(7);
+
+function copyright(int $year) {
+    return "&copy; $year BeCode";
+}
+//print the copyright
+ echo copyright((int)date('Y'));
+
+
+new_exercise(8);
+
+function login(string $email, string $password) {
+    
+    if($email == 'john@example.be' || $password == 'pocahontas') {
+         return 'Welcome John' . ' Smith';
+    }else{
+        return 'No access';
+    }
+    
+}
+//should great the user with his full name (John Smith)
+$login = login('john@example.be', 'pocahontas');
+//no access
+//$login = login('john@example', 'dfgidfgdfg');
+//no access
+//$login = login('wrong@example', 'wrong');
+
+echo $login;
+
+
+new_exercise(9);
+
+function isLinkValid(string $link) {
+    $unacceptables = array('https:','.doc','.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+
+    foreach ($unacceptables as $unacceptable) {
+        if (strpos($link, $unacceptable) == true) {
+            return 'Unacceptable Found<br />';
+        }
+    }
+    return 'Acceptable<br />';
+}
+//invalid link
+isLinkValid('http://www.google.com/hack.pdf');
+//invalid link
+isLinkValid('https://google.com');
+//VALID link
+isLinkValid('http://google.com');
+//VALID link
+isLinkValid('http://google.com/test.txt');
+
+new_exercise(10);
+
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+for($i=0; $i <= count($areTheseFruits); $i++) {
+    //echo $areTheseFruits[$i] . "<br/>";
+    if(!in_array($areTheseFruits[$i], $validFruits)) {
+        unset($areTheseFruits[$i]);
+    }
+}   
+array_pop($areTheseFruits);
+var_dump($areTheseFruits);//do not change this
