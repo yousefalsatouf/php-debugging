1a2856bceeecd5641fc58d719076181d1136ae3a
Yousef Alsatouf <yousef.alsatouf94@gmail.com>
2019-10-30 15:56:49 +0100

    almost done

--- expert.php
+++ expert.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 // === Exercise 1 ===
@@ -12,8 +13,9 @@ $x = 1;
 
 new_exercise(1);
 
-function new_exercise($x) {
-    $block = "<br/><hr/><br/><br/>Exercise ". $x ." starts here:<br/>";
+function new_exercise($x)
+{
+    $block = "<br/><hr/><br/><br/>Exercise " . $x . " starts here:<br/>";
     echo $block;
 }
 
@@ -44,12 +46,10 @@ new_exercise(4);
 // Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
 
 
-foreach($week as $day) {
-    $day = substr($day, 0, strlen($day)-3);
+foreach ($week as $day) {
+    $day = substr($day, 0, strlen($day) - 3);
 
-    $week .= $day . " ";  
-
-  
+    $week .= $day . " ";
 }
 
 print_r($week);
@@ -61,14 +61,13 @@ new_exercise(5);
 
 $arr = [];
 for ($letter = 'a'; $letter <= 'z'; $letter++) {
-    
+
     // if ( sizeof($arr) <= 25){
     //     array_push($arr, $letter);
     // }
 
     array_push($arr, $letter);
-    if($letter == "z") break;
-   
+    if ($letter == "z") break;
 }
 print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
 
@@ -81,9 +80,10 @@ new_exercise(6);
 $arr = [];
 
 
-function combineNames($str1 = "", $str2 = "") {
-    $params = [$str1, $str2];
-    foreach($params as $param) {
+function combineNames($str1 = "", $str2 = "")
+{
+    $params = [$str1,    $str2];
+    foreach ($params as $param) {
         if ($param == "") {
             $param = randomHeroName();
         }
@@ -92,7 +92,11 @@ function combineNames($str1 = "", $str2 = "") {
 }
 
 
-function randomGenerate($arr, $amount) {
+
+
+
+function randomGenerate($arr, $amount)
+{
     for ($i = $amount; $i > 0; $i--) {
         array_push($arr, randomHeroName());
     }
@@ -105,7 +109,7 @@ function randomHeroName()
     $hero_firstnames = ["captain", "doctor", "iron", "Hank", "ant", "Wasp", "the", "Hawk", "Spider", "Black", "Carol"];
     $hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil", "marvel"];
     $heroes = [$hero_firstnames, $hero_lastnames];
-    $randname = $heroes[rand(0,count($heroes))][rand(0, 10)];
+    $randname = $heroes[rand(0, count($heroes))][rand(0, 10)];
 
     echo $randname;
 }
@@ -116,54 +120,61 @@ echo "Here is the name: " . combineNames();
 
 new_exercise(7);
 
-function copyright(int $year) {
+function copyright(int $year)
+{
     return "&copy; $year BeCode";
 }
 //print the copyright
- echo copyright((int)date('Y'));
+echo copyright((int) date('Y'));
 
 
 new_exercise(8);
 
-function login(string $email, string $password) {
-    
-    if($email == 'john@example.be' || $password == 'pocahontas') {
-         return 'Welcome John' . ' Smith';
-    }else{
-        return 'No access';
+function login(string $email, string $password)
+{
+    //echo $password . "<br/>";
+    if ($email == 'john@example.be' && $password == 'pocahontas') {
+        return 'Welcome John' . ' Smith';
     }
-    
+    return 'No access';
 }
 //should great the user with his full name (John Smith)
 $login = login('john@example.be', 'pocahontas');
+echo "<br>$login";
 //no access
-//$login = login('john@example', 'dfgidfgdfg');
+$login = login('john@example', 'dfgidfgdfg');
+echo "<br>$login";
 //no access
-//$login = login('wrong@example', 'wrong');
+$login = login('wrong@example', 'wrong');
 
-echo $login;
-
+echo "<br>$login";
 
 new_exercise(9);
 
-function isLinkValid(string $link) {
-    $unacceptables = array('https:','.doc','.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+function isLinkValid(string $link)
+{
+    $unacceptables = array('https:', '.doc', '.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
 
     foreach ($unacceptables as $unacceptable) {
-        if (strpos($link, $unacceptable) == true) {
+
+        if (strpos($link, $unacceptable) !== false) {
             return 'Unacceptable Found<br />';
         }
     }
     return 'Acceptable<br />';
 }
 //invalid link
-isLinkValid('http://www.google.com/hack.pdf');
+$check = isLinkValid('http://www.google.com/hack.pdf');
+echo $check;
 //invalid link
-isLinkValid('https://google.com');
+$check = isLinkValid('https://google.com');
+echo $check;
 //VALID link
-isLinkValid('http://google.com');
+$check = isLinkValid('http://google.com');
+echo $check;
 //VALID link
-isLinkValid('http://google.com/test.txt');
+$check = isLinkValid('http://google.com/test.txt');
+echo $check;
 
 new_exercise(10);
 
@@ -172,11 +183,11 @@ new_exercise(10);
 $areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
 $validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
 //from here on you can change the code
-for($i=0; $i <= count($areTheseFruits); $i++) {
+for ($i = 0; $i <= count($areTheseFruits); $i++) {
     //echo $areTheseFruits[$i] . "<br/>";
-    if(!in_array($areTheseFruits[$i], $validFruits)) {
+    if (!in_array($areTheseFruits[$i], $validFruits)) {
         unset($areTheseFruits[$i]);
     }
-}   
+}
 array_pop($areTheseFruits);
 var_dump($areTheseFruits);//do not change this
